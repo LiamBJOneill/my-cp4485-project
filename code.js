9b53624f85f0ffaa3a75f4223a3da6a99c032e07
Liam <liamon4@gmail.com>
2024-12-28 12:09:50 -0330

    Many Commits to fix browser specific bug

--- code.js
+++ code.js
@@ -453,33 +453,6 @@ function timeOut() {
     }
 }
 
-function readScores() {
-    fetch("/High_Scores.csv").then(function (response) {
-        if (response.ok) {
-            return response.text();
-        } throw response;
-    }).then(function (text) {
-        console.log(text);
-    });
-}
-
-function saveJsonObjToFile() {
-    const saveObj = { "a": 3 }; // tmp
-
-    // file setting
-    const text = JSON.stringify(saveObj);
-    const name = "sample.json";
-    const type = "text/plain";
-
-    // create file
-    const a = document.createElement("a");
-    const file = new Blob([text], { type: type });
-    a.href = URL.createObjectURL(file);
-    a.download = name;
-    document.body.appendChild(a);
-    a.click();
-    a.remove();
-}
 
 
 function onBoot() {
